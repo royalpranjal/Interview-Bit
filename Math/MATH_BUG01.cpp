6eb6e1964a9132bfa34d31d0303f7579700804c1
Pranjal <ug201314013@iitj.ac.in>
2016-07-21 17:36:05 +0530

    Exa

---
+++ Math/MATH_BUG01.cpp
@@ -0,0 +1,17 @@
+// https://www.interviewbit.com/problems/math_bug01/
+
+// Return 1 if A is prime, else 0
+int Solution::isPrime(int A) {
+	int upperLimit = round(sqrt(A));
+    if(A > 1){
+        for (int i = 2; i <= upperLimit; i++) {
+            if (A % i == 0){
+                return 0;
+            }
+        }
+    }
+    else{
+        return 0;
+    }
+    return 1;
+}
