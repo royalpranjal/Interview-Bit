161327808c2f4a7b5fafd947296e89fd969c96f9
Pranjal <ug201314013@iitj.ac.in>
2016-08-23 15:51:47 +0530

    Facebook,Amazon,Microsoft

---
+++ Two-Pointers/3Sum.cpp
@@ -0,0 +1,43 @@
+// https://www.interviewbit.com/problems/3-sum/
+
+int Solution::threeSumClosest(vector<int> &A, int B) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+    if(A.size() < 3){
+        return 0;
+    }
+    
+    sort(A.begin(), A.end());
+    
+    int i = 0;
+    int sum, minDiff = INT_MAX;
+    int count = 0;
+    
+    while(i < A.size()-2){
+        int left = i+1;
+        int right = A.size()-1;
+        while(right > left){
+            int temp = A[i]+A[left]+A[right];
+            int diff = abs(temp-B);
+            if(diff == 0){
+                return B;
+            }
+            if(diff < minDiff){
+                minDiff = diff;
+                sum = temp;
+            }
+            if(temp < B){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        i++;
+    }
+    
+    return sum;
+}
