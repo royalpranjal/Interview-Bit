9709f20032f7744f4240aca0cba2e91c74845357
Pranjal <ug201314013@iitj.ac.in>
2017-11-02 22:11:46 +0530

    ashaywalke's concise solution
    - Keeping both the solutions in the file. 
    - ashaywalke's solution is concise & doesn't have any redundant code.

--- Math/GridUniquePaths.cpp
+++ Math/GridUniquePaths.cpp
@@ -1,14 +1,50 @@
 // https://www.interviewbit.com/problems/grid-unique-paths/
 
-class Solution {
-    public:
-        int uniquePaths(int m, int n) {
-            // m+n-2 C n-1 = (m+n-2)! / (n-1)! (m-1)! 
-            long long ans = 1;
-            for (int i = n; i < (m + n - 1); i++) {
-                ans *= i;
-                ans /= (i - n + 1);
-            }
-            return (int)ans;
-        }
-};
+// long long int permute(int x, int y){
+//     long long int sol = 1;
+//     for(int i = 1; i <= y; i++){
+//         sol = sol*(x-i+1);
+//     }
+//     return sol;
+// }
+
+// long long int fact(int x){
+//     if(x == 1){
+//         return 1;
+//     }
+//     else{
+//         return x*fact(x-1);
+//     }
+// }
+
+int Solution::uniquePaths(int A, int B) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+    // Solution 1 (concise solution)
+    // m+n-2 C n-1 = (m+n-2)! / (n-1)! (m-1)! 
+    long long ans = 1;
+    for (int i = B; i < (A + B - 1); i++) {
+        ans *= i;
+        ans /= (i - B + 1);
+    }
+    return (int)ans;
+    
+    // Solution 2 
+    // int x = A-1;
+    // int y = B-1;
+    
+    // if(x == 0 || y == 0){
+    //     return 1;
+    // }
+    
+    // int small = x;
+    // if(y < x){
+    //     small = y;
+    // }
+    // int ans = int (permute(x+y, small)/fact(small));
+    
+    // return ans;
+}
