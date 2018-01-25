03d966f91b8274896ae585c3249aa3baabe0b0dc
Pranjal <ug201314013@iitj.ac.in>
2018-01-25 19:32:59 +0530

    Counting TrianglesBookmark Suggest Edit

---
+++ Two-Pointers/Counting Triangles.cpp
@@ -0,0 +1,25 @@
+int Solution::nTriang(vector<int> &A) {
+    int ans = 0, n = A.size(), num = pow(10, 9) + 7;
+    
+    if(!n){
+        return ans;
+    }
+    
+    sort(A.begin(), A.end());
+    
+    for(int k = n - 1; k >= 0; k--){
+        int i = 0, j = k - 1;
+        while(i < j){
+            long int miniSum = A[i] + A[j], maxi = A[k];
+            if(miniSum > maxi){
+                ans = (ans + (j - i)%num)%num;
+                j--;
+            }   
+            else{
+                i++;    
+            }
+        }
+    }
+    
+    return ans;
+}
