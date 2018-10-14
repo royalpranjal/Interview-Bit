ed1355a56f6bd0c39f8bd180d34a2a5a9f386300
CodeLogist <33797980+CodeLogist@users.noreply.github.com>
2018-10-14 20:45:50 +0530

    FibSUM.cpp
    This one is with Better Space and Time complexity with not extra variable space or Vector. Please have a Look

--- Graphs/SumOfFibonacciNumbers.cpp
+++ Graphs/SumOfFibonacciNumbers.cpp
@@ -1,39 +1,26 @@
-// https://www.interviewbit.com/problems/sum-of-fibonacci-numbers/
+#include <iostream>
 
-int Solution::fibsum(int A) {
-    // Do not write main() function.
-    // Do not read input, instead use the arguments to the function.
-    // Do not print the output, instead return values as specified
-    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-    
-    vector<int> vec;
-    
-    vec.push_back(1);
-    vec.push_back(1);
-    
-    int fib, i = 2;
-    
-    while(fib <= A){
-        fib = vec[i-2] + vec[i-1];
-        vec.push_back(fib);
-        i++;
-    }
-    
-    int j = vec.size()-1;
-    int sol = 0;
-    
-    LOOP:while(A && j >= 0){
-        if(vec[j] == A){
-            sol++;
-            return sol;
-        }
-        else if(vec[j] < A){
-            sol++;
-            A = A - vec[j];
-            goto LOOP;
-        }
-        j--;
-    }
-    
+
+int main()// main fibonacci program
+{
+	int fib1 = 0;
+	int fib2 = 1;
+	int fib3 = 0;
+	int c;
+    int sum=0;
+	//decalare all variables
+	std::cout << "How many fibonacci numbers do you want to sum?" << std::endl;
+	std::cin >> c;
+	//get user input and print the first "0" and "1"
+	std::cout << fib1 << std::endl;;
+	std::cout << fib2 << std::endl;
+	for (int i = 0; i < c; i++)// Looping though the fibonacci algorithm
+	{
+		fib3 = fib1 + fib2;
+        sum += fib3;
+		fib1 = fib2;
+		fib2 = fib3;
+	}
+    std::cout<<sum<<endl;
     return 0;
 }
