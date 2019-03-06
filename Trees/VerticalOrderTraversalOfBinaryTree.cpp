a13c992c504ac95530b6bbf7753543ee6911f9e1
Sahil <yadavsahil5198@Gmail.com>
2019-03-06 14:34:07 +0530

    Update VerticalOrderTraversalOfBinaryTree.cpp
    Lesser number of lines in the new code

--- Trees/VerticalOrderTraversalOfBinaryTree.cpp
+++ Trees/VerticalOrderTraversalOfBinaryTree.cpp
@@ -7,74 +7,103 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-struct node{
-    TreeNode* root;
-    int dis;
-};
-
-void make(map<int, vector<int> >& m, queue<node>& q){
-    if(q.size() == 0){
-        return;
-    }
-    
-    TreeNode* root = (q.front()).root;
-    int dis = (q.front()).dis;
-    
-    if(!root){
-        return;
-    }
-    
-    m[dis].push_back(root->val);
-    
-    q.pop();
-    
-    if(root->left){
-        node l1;
-        l1.root =  root->left;
-        l1.dis = dis-1;
-        q.push(l1);
-    }
-    
-    if(root->right){
-        node l1;
-        l1.root =  root->right;
-        l1.dis = dis+1;
-        q.push(l1);
-    }
-    
-    make(m, q);
-}
 
 vector<vector<int> > Solution::verticalOrderTraversal(TreeNode* A) {
-    map<int, vector<int> > m;
-    vector<vector<int> > sol;
-    queue<node> q;
+    vector<vector<int> > v;
+    if(A == NULL)return v;
     
-    node n;
-    n.root = A;
-    n.dis = 0;
+    queue<pair <TreeNode*, int> > q;
+    q.push({A, 0});
     
-    q.push(n);
-    
-    make(m, q);
-    
-    map<int, vector<int> > :: iterator it = m.begin();
-    
-    int curr = 0;
-    
-    while(it != m.end()){
-        vector<int> temp;
-        sol.push_back(temp);
+    map<int, vector<int> > mp;
+    while(!q.empty()){
+        pair<TreeNode*, int> temp = q.front();
+        q.pop();
         
-        temp = it->second;
-        
-        for(int i = 0; i < temp.size(); i++){
-            sol[curr].push_back(temp[i]);
+        if(temp.first->left){
+            q.push({temp.first->left, temp.second-1});
+        }
+        if(temp.first->right){
+            q.push({temp.first->right, temp.second+1});
         }
         
-        curr++;
-        it++;
+        mp[temp.second].push_back(temp.first->val);
     }
-    
-    return sol;
+
+    for(map<int, vector<int> >::iterator it = mp.begin(); it != mp.end(); it++){
+        v.push_back(it->second);
+    }
+    return v;
 }
+
+// struct node{
+//     TreeNode* root;
+//     int dis;
+// };
+
+// void make(map<int, vector<int> >& m, queue<node>& q){
+//     if(q.size() == 0){
+//         return;
+//     }
+    
+//     TreeNode* root = (q.front()).root;
+//     int dis = (q.front()).dis;
+    
+//     if(!root){
+//         return;
+//     }
+    
+//     m[dis].push_back(root->val);
+    
+//     q.pop();
+    
+//     if(root->left){
+//         node l1;
+//         l1.root =  root->left;
+//         l1.dis = dis-1;
+//         q.push(l1);
+//     }
+    
+//     if(root->right){
+//         node l1;
+//         l1.root =  root->right;
+//         l1.dis = dis+1;
+//         q.push(l1);
+//     }
+    
+//     make(m, q);
+// }
+
+// vector<vector<int> > Solution::verticalOrderTraversal(TreeNode* A) {
+//     map<int, vector<int> > m;
+//     vector<vector<int> > sol;
+//     queue<node> q;
+    
+//     node n;
+//     n.root = A;
+//     n.dis = 0;
+    
+//     q.push(n);
+    
+//     make(m, q);
+    
+//     map<int, vector<int> > :: iterator it = m.begin();
+    
+//     int curr = 0;
+    
+//     while(it != m.end()){
+//         vector<int> temp;
+//         sol.push_back(temp);
+        
+//         temp = it->second;
+        
+//         for(int i = 0; i < temp.size(); i++){
+//             sol[curr].push_back(temp[i]);
+//         }
+        
+//         curr++;
+//         it++;
+//     }
+    
+//     return sol;
+// }
