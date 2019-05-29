cdde06806b4713c20567d4fb16b36bc66db7b248
Shivam Gupta <shingwl@yahoo.com>
2019-05-29 15:08:50 +0530

    better and smaller Solution for Trees/MinimumDepthOfABinaryTree.cpp

--- Trees/MinimumDepthOfABinaryTree.cpp
+++ Trees/MinimumDepthOfABinaryTree.cpp
@@ -1,5 +1,3 @@
-// https://www.interviewbit.com/problems/min-depth-of-binary-tree/
-
 /**
  * Definition for binary tree
  * struct TreeNode {
@@ -9,28 +7,8 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-int depth(TreeNode* root){
-    if(root == NULL){
-        return 0;
-    }
-    else if(root->left == NULL && root->right == NULL){
-        return 1;
-    }
-    else if(root->left == NULL && root->right != NULL){
-        return 1 + depth(root->right);
-    }
-    else if(root->left != NULL && root->right == NULL){
-        return 1 + depth(root->left);
-    }
-    return min(1 + depth(root->right), 1 + depth(root->left));
-}
-
 int Solution::minDepth(TreeNode* A) {
-    // Do not write main() function.
-    // Do not read input, instead use the arguments to the function.
-    // Do not print the output, instead return values as specified
-    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-
-    return depth(A);
-    
+    if(A==NULL) return INT_MAX;
+    if(A->left==NULL && A->right==NULL) return 1;
+    return min(minDepth(A->left),minDepth(A->right))+1;
 }
