402c5f7d6340e5b422356eaad8ad61408fe281ae
Hardik Rana <hardikrana276@gmail.com>
2019-06-05 11:23:33 +0200

    reduced code size

--- Math/ExcelColumnTitle.cpp
+++ Math/ExcelColumnTitle.cpp
@@ -5,7 +5,27 @@ string Solution::convertToTitle(int A) {
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-
+    
+    string str = "";
+    int i = 0; 
+    while (A>0) 
+    { 
+        int rem = A%26; 
+        if (rem==0) 
+        { 
+            str.push_back('Z');
+            A = (A/26)-1; 
+        } 
+        else 
+        { 
+            str.push_back((rem-1) + 'A');
+            A = A/26; 
+        } 
+    } 
+    reverse(str.begin(), str.end()); 
+    return str;
+    
+    /*
     int diff, i = 0, j = 0, length = 0, sum = 0, flag = 0;
     string ans;
     
@@ -37,4 +57,5 @@ string Solution::convertToTitle(int A) {
     }
     
     return ans;
+    */
 }
