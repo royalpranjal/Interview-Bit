aa81f0c6d95c2f87c1ad37f523f3b2f8f3d378f4
Hardik Rana <hardikrana276@gmail.com>
2019-06-15 08:45:15 +0200

    reduced code size

--- Stacks-and-Queues/NearestSmallestElement.cpp
+++ Stacks-and-Queues/NearestSmallestElement.cpp
@@ -1,5 +1,5 @@
 // https://www.interviewbit.com/problems/nearest-smaller-element/
-
+/*
 vector<int> Solution::prevSmaller(vector<int> &A) {
 
     if(A.size() == 0){
@@ -39,5 +39,28 @@ vector<int> Solution::prevSmaller(vector<int> &A) {
         }
     }
     
+    return ans;
+}
+*/
+
+vector<int> Solution::prevSmaller(vector<int> &a) {
+    
+    stack<int> s;
+    vector<int> ans;
+    ans.push_back(-1);
+    s.push(a[0]);
+    
+    for(int i=1;i<a.size();i++)
+    {
+        while(!s.empty() && s.top()>=a[i])
+        s.pop();
+        
+        if(s.empty())
+        ans.push_back(-1);
+        else
+        ans.push_back(s.top());
+        
+        s.push(a[i]);
+    }
     return ans;
 }
