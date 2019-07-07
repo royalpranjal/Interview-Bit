142ebaaca48301937b3cae16591d7a10d594c08f
Hardik Rana <hardikrana276@gmail.com>
2019-07-07 07:00:18 +0200

    Palindrome String

--- Strings/PalindromeString.cpp
+++ Strings/PalindromeString.cpp
@@ -1,6 +1,44 @@
 // https://www.interviewbit.com/problems/palindrome-string/
 
 int Solution::isPalindrome(string A) {
+    
+    int len = A.length();
+    
+    for(int i=0;i<len;i++)
+    {
+        A[i] = tolower(A[i]);
+    }
+    
+    int l = 0;
+    int h = len;
+    
+    while(l<=h)
+    {
+        if(!(A[l]>='a' && A[l]<='z') && !(A[l]>='0' && A[l]<='9'))
+        {
+            l++;
+        }
+        
+        else if(!(A[h]>='a' && A[h]<='z') && !(A[h]>='0' && A[h]<='9'))
+        {
+            h--;
+        }
+        else if(A[l] == A[h])
+        {
+            l++;
+            h--;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    
+    return 1;
+}
+
+/*
+int Solution::isPalindrome(string A) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
@@ -85,3 +123,4 @@ int Solution::isPalindrome(string A) {
     
     return 1;
 }
+*/
