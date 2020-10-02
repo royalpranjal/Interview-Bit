6eca5f9a839f6a1da1d3fdc9761b43e432861f5d
naveen_ky@hotmail.com <696787nit>
2020-10-02 23:44:39 +0530

    interviewbit: combinational sum2
    Updated the exisitng code more optimise version. The exisiting one is gettin time out on leetcode for similar problem.
    
    https://leetcode.com/problems/combination-sum-ii/

--- Backtracking/CombinationSumII.cpp
+++ Backtracking/CombinationSumII.cpp
@@ -1,44 +1,30 @@
-void make(vector<int>& A, int curr, int currSum, int sum, vector<int> temp, vector<vector<int> >& ans, map<vector<int>, bool>& m){
-    if(currSum > sum){
-        return;
-    }    
-    else if(currSum == sum){
-        if(m.find(temp) == m.end()){
-            m[temp] = true;
-            ans.push_back(temp);
-        }
+void back_track(vector<vector<int>> &ans, vector<int> &combination, vector<int>& candidates, int start, int target) {
+    if (target == 0) {
+        ans.push_back(combination);
         return;
     }
-    
-    for(int i = curr; i < A.size(); i++){
-        vector<int> t(temp);
-        t.push_back(A[i]);
-        make(A, i+1, currSum+A[i], sum, t, ans, m);
+        
+    for ( int i = start; i < candidates.size(); i++) {
+        if ((target - candidates[i]) < 0)
+            return;
+        if (candidates[i] <= target) {
+            combination.push_back(candidates[i]);
+            back_track(ans, combination, candidates, i + 1, target - candidates[i]);
+            combination.pop_back();  
+            while(i+1<candidates.size() && candidates[i]==candidates[i+1]) {
+                i++;
+            }
+        }
     }
 }
 
 vector<vector<int> > Solution::combinationSum(vector<int> &A, int B) {
-    // Do not write main() function.
-    // Do not read input, instead use the arguments to the function.
-    // Do not print the output, instead return values as specified
-    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-
-    vector<vector<int> > ans;
-    
-    int n = A.size();
-    
-    if(A.size() == 0){
-        return ans;
-    }
-    
+	vector<vector<int>> ans;
+    vector<int> combination;
+    vector<int>::iterator ip;
+	
     sort(A.begin(), A.end());
-    map<vector<int>, bool> m;
+    back_track(ans, combination, A, 0, B);
     
-    for(int i = 0; i < n; i++){
-        vector<int> temp;
-        temp.push_back(A[i]);
-        make(A, i+1, A[i], B, temp, ans, m);
-    }
-    
-    return ans;
+	return ans;
 }
