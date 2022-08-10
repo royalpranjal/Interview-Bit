f8bec1095ae8fe87b4e594bb4a97da0fec2a4760
nilesh05apr <nk8588338@gmail.com>
2022-08-10 05:50:08 +0530

    removed redundant part of code, added one base case

--- DynamicProgramming/BestTimeToBuyAndSellStocks-II.cpp
+++ DynamicProgramming/BestTimeToBuyAndSellStocks-II.cpp
@@ -6,9 +6,10 @@ int Solution::maxProfit(const vector<int> &A) {
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
     
-    vector<int> temp(A.size(), 0);
+    // vector<int> temp(A.size(), 0);
     
-    int buy = A[0], flag = 0, i = 1, max_sell = INT_MIN;
+    // int buy = A[0], flag = 0, i = 1, max_sell = INT_MIN;
+    if(A.size() == 0 || A.size() == 1) return 0;
     int sol = 0;
     while(i < A.size()){
         int diff = A[i] - A[i-1];
